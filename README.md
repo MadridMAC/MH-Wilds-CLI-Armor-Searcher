ef4b8e09d8e035a19bce9d1d7f67263c7867e347
MadridMAC <anymordonutzarty@gmail.com>
2025-03-16 22:33:44 +0800

    Update: Added protection against invalid input

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # MH Wilds CLI Armor Searcher
-A simple Python project that reads a .csv file containing all armor sets from Monster Hunter Wilds, accepts a valid armor skill as input, and returns all known armor pieces containing that armor skill.
-Currently a work in progress. I plan on adding more features.
+A simple Python project that reads a .csv file containing all armor sets from Monster Hunter Wilds, accepts a valid armor skill as input, and returns all known armor pieces containing that armor skill.\
+Note that this project is currently a work-in-progress.\
+The CSV file in the repo only contains sample armor data for the time being.
+# Latest Changes
+## Version 1.1
+Added protection against invalid input.
--- csv_reading.py
+++ csv_reading.py
@@ -4,11 +4,21 @@ def print_armor(armor):
     print(f"Name: {armor[0]} // Armor Slot: {armor[1]}\nDeco Slots: {armor[2]} // Skills: {armor[3]}")
     print("===========x==========")
 
+def init_checker(csv_list, skill_name):
+    contains_skill = False
+    for item in csv_list:
+        if skill_name in item[3]:
+            contains_skill = True
+    return contains_skill
+
 def main():
     skill_to_find = input("Enter armor skill to find: ")
     print("===========x==========")
     with open('armordata.csv', newline='') as csvfile:
         listed_file = list(csv.reader(csvfile))
+        if (not init_checker(listed_file, skill_to_find)):
+            print("Skill not found")
+            return
         for item in listed_file:
             if (skill_to_find in item[3]):
                 print_armor(item)
