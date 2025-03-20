37439a2a51357a1ebb4b74f4521361431c0e20b6
MadridMAC <anymordonutzarty@gmail.com>
2025-03-20 12:58:44 +0800

    Version 2: Implemented  AND, OR, and SINGLE searching functionality

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
 # MH Wilds CLI Armor Searcher
-A simple Python project that reads a .csv file containing all armor sets from Monster Hunter Wilds, accepts a valid armor skill as input, and returns all known armor pieces containing that armor skill.\
-Note that this project is currently a work-in-progress.\
-The CSV file in the repo only contains sample armor data for the time being.
-# Latest Changes
-## Version 1.1
-Added protection against invalid input.
+A simple Python project that reads a .csv file containing all armor sets from Monster Hunter Wilds, accepts a valid armor skill as input, and returns all known armor pieces containing that armor skill.<br>
+I've limited the CSV file to only contain High Rank sets. Feel free to use it as a template and populate it with other sets.\
+The CSV file also only uses Name, Armor Slot, Deco Slots, and Skills, so you should be able to adopt it for use with other games as well.
+## How to Use
+Before anything else, this script requires **Python 3.12.3**.<br><br>
+First, specify whether you want to use SINGLE, OR, or AND skill searching:
+
+- SINGLE will look for armor pieces containing the skill you input
+- AND will look for armor pieces containing both of the two skills you input
+- OR will look for armor pieces containing either of the two skills you input
+
+Next, input the skill/s you want to search for as requested by the script.\
+The program will then return the armor pieces fitting your criteria.<br>
+
+To terminate the program, type in "exit" (case insensitive) for each requested input.
+
+## Latest Changes
+### To-Do
+- Fill out armordata.csv with all current High Rank armor sets (pre-TU1)
+### Version 2
+- Added AND, OR, and SINGLE searching functionality
+### Version 1.1
+- Added some catches for invalid or incorrect input.
+### Version 1.0
+- Project release into GitHub.
--- csv_reading.py
+++ csv_reading.py
@@ -1,9 +1,11 @@
 import csv
 
+# Beautifies the CSV Armor row and makes it easier to read
 def print_armor(armor):
-    print(f"Name: {armor[0]} // Armor Slot: {armor[1]}\nDeco Slots: {armor[2]} // Skills: {armor[3]}")
+    print(f"Armor Name: {armor[0]} // Armor Slot: {armor[1]}\nDeco Slots: {armor[2]} // Skills: {armor[3]}")
     print("===========x==========")
 
+# Initially check through the entire CSV file to see if the provided skill name exists
 def init_checker(csv_list, skill_name):
     contains_skill = False
     for item in csv_list:
@@ -11,16 +13,66 @@ def init_checker(csv_list, skill_name):
             contains_skill = True
     return contains_skill
 
-def main():
-    skill_to_find = input("Enter armor skill to find: ")
-    print("===========x==========")
+# Searches for a single skill in the armor list
+def single_armor_searcher(skill_name):
     with open('armordata.csv', newline='') as csvfile:
         listed_file = list(csv.reader(csvfile))
-        if (not init_checker(listed_file, skill_to_find)):
-            print("Skill not found")
-            return
+        if (not init_checker(listed_file, skill_name)):
+            print("Skill not found - Ensure that you've typed a valid skill name (case sensitive)")
         for item in listed_file:
-            if (skill_to_find in item[3]):
+            if (skill_name in item[3]):
                 print_armor(item)
+
+# Searches for both skills on a single piece of armor in the list
+def and_armor_searcher(skill_1, skill_2):
+    with open('armordata.csv', newline='') as csvfile:
+        listed_file = list(csv.reader(csvfile))
+        if ((not init_checker(listed_file, skill_1)) or (not init_checker(listed_file, skill_2))):
+            print("Skill not found - Ensure that you've typed a valid skill name (case sensitive)")
+        for item in listed_file:
+            if ((skill_1 in item[3]) and (skill_2 in item[3])):
+                print_armor(item)
+
+# Searches for either skill on a single piece of armor in the list
+def or_armor_searcher(skill_1, skill_2):
+    with open('armordata.csv', newline='') as csvfile:
+        listed_file = list(csv.reader(csvfile))
+        if ((not init_checker(listed_file, skill_1)) or (not init_checker(listed_file, skill_2))):
+            print("Skill not found - Ensure that you've typed a valid skill name (case sensitive)")
+        for item in listed_file:
+            if ((skill_1 in item[3]) or (skill_2 in item[3])):
+                print_armor(item)
+
+def main():
+    # Check if user wants AND, OR, or a SINGLE armor skill
+    search_type = input("AND, OR, or SINGLE? ")
+    if (search_type.upper() not in ["AND", "OR", "SINGLE"] ):
+        print("Invalid input. Exiting...")
+        return
+    print("For each input, type EXIT to quit.")
+    # While loop lets user search multiple times for the search type they chose
+    while True:
+        # Switch cases for SINGLE, AND, and OR searching
+        match (search_type.upper()):
+            case "SINGLE":
+                skill_to_find = input("Enter armor skill to find: ")
+                if (skill_to_find.upper() == "EXIT"):
+                    return
+                print("===========x==========")
+                single_armor_searcher(skill_to_find)
+            case "AND":
+                skill_1 = input("Enter first skill to find: ")
+                skill_2 = input("Enter second skill to find: ")
+                if ((skill_1.upper() == "EXIT") or (skill_2.upper() == "EXIT")):
+                    return
+                print("===========x==========")
+                and_armor_searcher(skill_1, skill_2)
+            case "OR":
+                skill_1 = input("Enter first skill to find: ")
+                skill_2 = input("Enter second skill to find: ")
+                if ((skill_1.upper() == "EXIT") or (skill_2.upper() == "EXIT")):
+                    return
+                print("===========x==========")
+                or_armor_searcher(skill_1, skill_2)
 
 main()
